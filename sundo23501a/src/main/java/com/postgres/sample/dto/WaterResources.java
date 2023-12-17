ae58f82219f1a158295843329d3260652d80376f
이광현 <rhkd4129@naver.com>
2023-12-17 19:29:42 +0900

    sibal

--- sundo23501a/src/main/java/com/postgres/sample/dto/WaterResources.java
+++ sundo23501a/src/main/java/com/postgres/sample/dto/WaterResources.java
@@ -11,26 +11,9 @@ public class WaterResources {
 	private String	facility_addr;
 	private String	facility_type;
 
-	private String 	org_code;	//운영기관
-	private String 	org_area;  //행정구역 코드
-	
+	private String 	org_code;
 	private Double 	latitude;
 	private Double 	longitude;
-	private String 	create_datetime;
-	private String 	modify_datetime;
-	
-
-	//조회용
-	private String org_area_name;// 행정기관 이름
-	private String cate_name; 		// ex) 목재댐
-	
-	private String org_name;	//운영기관 이름
-	
-	private String rn;
-	private String pageNum;		private int total;
-	private int start; 		 	private int end;
-	private String search;   	private String keyword;
-
-	
-
+	private Date 	create_datetime;
+	private Date 	modify_datetime;
 }
--- sundo23501a/src/main/java/com/postgres/sample/service/impl/dao/kjo/BoardDAOv2.java
+++ sundo23501a/src/main/java/com/postgres/sample/service/impl/dao/kjo/BoardDAOv2.java
@@ -2,13 +2,29 @@ package com.postgres.sample.service.impl.dao.kjo;
 
 import java.util.List;
 
-import org.egovframe.rte.psl.dataaccess.EgovAbstractDAO;
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
 import com.postgres.sample.dto.BoardVO;
 
-@Repository
-public interface  BoardDAOv2 {
+import lombok.RequiredArgsConstructor;
 
-    List<BoardVO> SelectBoardList();
+@Repository
+@RequiredArgsConstructor
+public class BoardDAOv2 {
+	private final SqlSession session;
+	
+	public List<BoardVO> SelectBoardList() {
+		List<BoardVO> boardList = null;
+		
+		try {
+			//-------------------------------------------------
+			boardList = session.selectList("test");
+			//-------------------------------------------------
+		} catch (Exception e) {
+			System.out.println("Exception->"+e.getMessage());
+		}
+		
+		return boardList;
+	}
 }
